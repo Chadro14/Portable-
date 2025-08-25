8f49957d3e59b5b2e5ed95e2161ca63cfbd0215c
Altesse Royal <altessemwamboko@gmail.com>
2025-08-25 02:02:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-// Import du serveur pour garder le bot en vie
-require('./server.js');
-
-// Code de votre bot Royale-Protection
 const qrcode = require('qrcode-terminal');
 const { Client, LocalAuth } = require('whatsapp-web.js');
 
+// La stratégie LocalAuth va sauvegarder la session dans un dossier
+// C'est essentiel pour que Render conserve votre connexion
 const client = new Client({
-    authStrategy: new LocalAuth()
+    authStrategy: new LocalAuth({
+        dataPath: './.wwebjs_auth'
+    })
 });
 
 client.on('qr', qr => {
+    // Affiche le code QR pour la connexion initiale
+    console.log('Veuillez scanner ce code QR pour connecter Royale-Protection:');
     qrcode.generate(qr, { small: true });
 });
 
@@ -18,7 +20,7 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    // Le code pour la détection de spam ira ici
+    // Le code pour la détection de spam et les actions ira ici
     console.log(`Message reçu de : ${message.from}`);
     console.log(`Contenu : ${message.body}`);
 });

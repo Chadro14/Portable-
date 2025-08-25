e2bb45228b2d412113cae30442b0700797af4977
Altesse Royal <altessemwamboko@gmail.com>
2025-08-25 01:56:20 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const qrcode = require('qrcode-terminal');
+const { Client, LocalAuth } = require('whatsapp-web.js');
+
+const client = new Client({
+    authStrategy: new LocalAuth()
+});
+
+client.on('qr', qr => {
+    qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+    console.log('Client is ready! Royale-Protection est maintenant en ligne et protège votre compte.');
+});
+
+client.on('message', message => {
+    // Le code pour la détection de spam ira ici plus tard
+    console.log(`Message reçu de : ${message.from}`);
+    console.log(`Contenu : ${message.body}`);
+});
+
+client.initialize();
